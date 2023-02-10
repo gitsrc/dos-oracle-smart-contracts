bf32c779184cfb91b44ff7413d7b1c542624de12
gitsrc <corerman@gmail.com>
2023-02-10 16:34:11 +0800

    feat(doc):learn example of coinbase ethusd contracts
    Signed-off-by: gitsrc <corerman@gmail.com>

--- contracts/examples/CoinbaseEthUsd.sol
+++ contracts/examples/CoinbaseEthUsd.sol
@@ -5,24 +5,25 @@ import "../DOSOnChainSDK.sol";
 
 // An example get latest ETH-USD price from Coinbase
 contract CoinbaseEthUsd is DOSOnChainSDK {
-    using StringUtils for *;
+    using StringUtils for *; //using dos network string lib
 
     // Struct to hold parsed floating string "123.45"
+    // define price data struct
     struct ethusd {
-        uint integral;
-        uint fractional;
+        uint integral; // integer part
+        uint fractional; //decimal part
     }
-    uint queryId;
+    uint queryId; //id of callback result
     string public price_str;
     ethusd public prices;
 
-    event GetPrice(uint integral, uint fractional);
+    event GetPrice(uint integral, uint fractional); //evm event
 
     constructor() public {
         // @dev: setup and then transfer DOS tokens into deployed contract
         // as oracle fees.
         // Unused fees can be reclaimed by calling DOSRefund() in the SDK.
-        super.DOSSetup();
+        super.DOSSetup(); 
     }
 
     function check() public {
@@ -30,14 +31,14 @@ contract CoinbaseEthUsd is DOSOnChainSDK {
     }
 
     function __callback__(uint id, bytes calldata result) external auth {
-        require(queryId == id, "Unmatched response");
+        require(queryId == id, "Unmatched response"); //if queryId wrong then end func flow
 
         price_str = string(result);
-        prices.integral = price_str.subStr(1).str2Uint();
-        uint delimit_idx = price_str.indexOf('.');
-        if (delimit_idx != result.length) {
+        prices.integral = price_str.subStr(1).str2Uint(); // get integer part of price data
+        uint delimit_idx = price_str.indexOf('.'); //get decimal part of price data
+        if (delimit_idx != result.length) { // if have pointer of price data then covert decimal part
             prices.fractional = price_str.subStr(delimit_idx + 1).str2Uint();
         }
-        emit GetPrice(prices.integral, prices.fractional);
+        emit GetPrice(prices.integral, prices.fractional); //send evm event 
     }
 }
